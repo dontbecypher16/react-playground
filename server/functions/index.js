29f4a42baff2a92e116103aac7bc47d10e91fb9f
Richard Dixon <rvictordixon@gmail.com>
2022-02-14 11:49:17 -0500

    started on endpoints for users

--- server/functions/index.js
+++ server/functions/index.js
@@ -8,8 +8,37 @@ const {
     editTodo
 } = require('./APIs/todos')
 
+const {
+    loginUser
+} = require('./APIs/users')
+
+
+
+
 app.get('/todos', getAllTodos);
 app.post('/todos', postOneTodo);
 app.delete('/todos/:todoId', deleteTodo);
 app.put('/todos/:todoId', editTodo);
+
+app.post('/login', loginUser)
+
 exports.api = functions.https.onRequest(app);
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAUwS6t2efNo546oW_LiAw-KtI49x8a7Xk",
+//   authDomain: "todo-5a124.firebaseapp.com",
+//   projectId: "todo-5a124",
+//   storageBucket: "todo-5a124.appspot.com",
+//   messagingSenderId: "595590741678",
+//   appId: "1:595590741678:web:88054ec07200e4d69f373f"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
---
+++ server/functions/utils/config.js
@@ -0,0 +1,9 @@
+
+exports.firebaseConfig = {
+    apiKey: "AIzaSyAUwS6t2efNo546oW_LiAw-KtI49x8a7Xk",
+    authDomain: "todo-5a124.firebaseapp.com",
+    projectId: "todo-5a124",
+    storageBucket: "todo-5a124.appspot.com",
+    messagingSenderId: "595590741678",
+    appId: "1:595590741678:web:88054ec07200e4d69f373f"
+};
